6f1187ba470daae2afb788f21522311783d0910c
Ragini Vanakhade <raginivankhade1203@gmail.com>
2026-07-25 16:41:30 +0530

    added nav bar component only

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import NavBar from './component/NavBar'
 
 function App() {
 
   return (
     <>
+    <NavBar/>
      <Routes>
       <Route path='home' element={"hello"}/>
      </Routes>
---
+++ src/component/NavBar.jsx
@@ -0,0 +1,10 @@
+import "../style/NavBar.css"
+const NavBar = () => {
+  return (
+    <div className="Nav-main">
+      Nav
+    </div>
+  )
+}
+
+export default NavBar
