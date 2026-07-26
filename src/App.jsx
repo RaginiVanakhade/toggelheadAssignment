cfe30509008b319352ca79e7f8c324980c580b7c
Ragini Vanakhade <raginivankhade1203@gmail.com>
2026-07-26 17:24:38 +0530

    css fix import

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,86 @@
 
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-// import NavBar from './component/NavBar'
+import NavBar from './component/NavBar'
 import HomeSection from './component/HomeSection'
 import HomeSection2 from './component/custom/HomeSection2'
 import CardSlider from './component/CarsSlider'
 import StatsSection from './component/StatusSection'
 import Faq from './component/Faq'
 import Footer from './component/Footer'
-
+import "./style/NavBar.css"
 // navbar 
-import { useState, useEffect, useRef } from "react";
-// import "../style/NavBar.css";
-import { IoIosArrowDown } from "react-icons/io";
-import Logo from "./assets/LogoLorem.avif"
-import CustomButton from "./component/custom/CustomBtn"
-import MegaMenu from "./component/MegaMenu";
-import menuData from "./component/MegaMenu"
+// import { useState, useEffect, useRef } from "react";
+// // import "../style/NavBar.css";
+// import { IoIosArrowDown } from "react-icons/io";
+// import Logo from "./assets/LogoLorem.avif"
+// import CustomButton from "./component/custom/CustomBtn"
+// import MegaMenu from "./component/MegaMenu";
+// import menuData from "./component/MegaMenu"
 
 
-const navItems = [
-  {
-    id: 1,
-    title: "Qualifications",
-    menuKey: "Qualifications",
-    hasDropdown: true,
-  },
-  {
-    id: 2,
-    title: "Organizations",
-    menuKey: "Organizations",
-    hasDropdown: true,
-  },
-  {
-    id: 3,
-    title: "Research & Analysis",
-    menuKey: "ResearchAnalysis",
-    hasDropdown: true,
-  },
-  {
-    id: 4,
-    title: "Lorem ipsum",
-    menuKey: "Lorem1",
-    hasDropdown: true,
-  },
-  {
-    id: 5,
-    title: "Lorem ipsum",
-    menuKey: "Lorem2",
-    hasDropdown: true,
-  },
-];
+// const navItems = [
+//   {
+//     id: 1,
+//     title: "Qualifications",
+//     menuKey: "Qualifications",
+//     hasDropdown: true,
+//   },
+//   {
+//     id: 2,
+//     title: "Organizations",
+//     menuKey: "Organizations",
+//     hasDropdown: true,
+//   },
+//   {
+//     id: 3,
+//     title: "Research & Analysis",
+//     menuKey: "ResearchAnalysis",
+//     hasDropdown: true,
+//   },
+//   {
+//     id: 4,
+//     title: "Lorem ipsum",
+//     menuKey: "Lorem1",
+//     hasDropdown: true,
+//   },
+//   {
+//     id: 5,
+//     title: "Lorem ipsum",
+//     menuKey: "Lorem2",
+//     hasDropdown: true,
+//   },
+// ];
 
 function App() {
 
-    const [menuOpen, setMenuOpen] = useState(false);
-  const [activeMenu, setActiveMenu] = useState(null);
+  //   const [menuOpen, setMenuOpen] = useState(false);
+  // const [activeMenu, setActiveMenu] = useState(null);
 
-  const navRef = useRef(null);
+  // const navRef = useRef(null);
 
-  useEffect(() => {
-    const handleClickOutside = (e) => {
-      if (navRef.current && !navRef.current.contains(e.target)) {
-        setActiveMenu(null);
-      }
-    };
+  // useEffect(() => {
+  //   const handleClickOutside = (e) => {
+  //     if (navRef.current && !navRef.current.contains(e.target)) {
+  //       setActiveMenu(null);
+  //     }
+  //   };
 
-    document.addEventListener("click", handleClickOutside);
+  //   document.addEventListener("click", handleClickOutside);
 
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, []);
+  //   return () => {
+  //     document.removeEventListener("click", handleClickOutside);
+  //   };
+  // }, []);
 
-  const handleMenuClick = (menuKey) => {
-    setActiveMenu((prev) => (prev === menuKey ? null : menuKey));
-  };
+  // const handleMenuClick = (menuKey) => {
+  //   setActiveMenu((prev) => (prev === menuKey ? null : menuKey));
+  // };
 
 
   return (
     <>
-      <header className="navbar" ref={navRef}>
+      {/* <header className="navbar" ref={navRef}>
           <div className="navbar-container">
     
     
@@ -132,8 +132,8 @@ function App() {
               onClose={() => setActiveMenu(null)}
             />
           )}
-        </header>
-    {/* <NavBar/> */}
+        </header> */}
+    <NavBar/>
     <HomeSection/>
     <HomeSection2/>
     <CardSlider/>
